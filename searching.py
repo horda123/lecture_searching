93c8730973e8c1250574e28ef1852069bb217b6b
horda123 <279092@vutbr.cz>
2026-04-15 10:20:20 +0200

    Add files via upload

--- searching.py
+++ searching.py
@@ -1,37 +1,52 @@
 from pathlib import Path
 import json
 
+def binary_search(sekvence, number):
+    while len(sekvence) > 0:
+        mid = len(sekvence)//2
+        if number == sekvence[mid]:
+            return True
+        elif number < sekvence[mid]:
+            sekvence = sekvence[:mid]
+        elif number > sekvence[mid]:
+            sekvence = sekvence[mid + 1:]
+    return False
+
+def linear_search(sekvence, number):
+    if not sekvence:
+        return []
+
+    indices = []
+    for index, value in enumerate(sekvence):
+        if value == number:
+            indices.append(index)
+    return indices
 
 def read_data(file_name, field):
-    """
-    Reads a JSON file and returns data for a given field.
-
-    Args:
-        file_name (str): Name of the JSON file.
-        field (str): Key to retrieve from the JSON data.
-            Must be one of: 'unordered_numbers', 'ordered_numbers' or 'dna_sequence'.
-
-    Returns:
-        list | str | None:
-            - list: If data retrieved by the selected field contains numeric data.
-            - str: If field is 'dna_sequence'.
-            - None: If the field is not supported.
-    """
-    # get current working directory path
     cwd_path = Path.cwd()
-    
     file_path = cwd_path / file_name
 
-    with open(filepath, "r") as file:
-        data = json.load(file)
-    if field in data.kezs():
-        return data(field)
+    with open(file_path, "r") as file:
+         data = json.load(file)
+
+    if field in data:
+        return data[field]
     else:
         return None
 
 def main():
+    file_name = "sequential.json"
+    field = "ordered_numbers"
+    number = 14
     sv_data = read_data(file_name, field)
-    print(sv_data)
+
+    if sv_data is not None:
+        print(f"Načtená data: {sv_data}")
+        vysledek = linear_search(sv_data, number)
+        print(f"Číslo {number} nalezeno sekvencne na indexech: {vysledek}")
+        bin_ser = binary_search(sv_data, number)
+        print(bin_ser)
+
 
 if __name__ == "__main__":
     main()
