85cb248b36a59fdfb3688fad460ae987d8dc7884
horda123 <279092@vutbr.cz>
2026-04-15 10:36:56 +0200

    Add files via upload

--- searching.py
+++ searching.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import time
 
 def binary_search(sekvence, number):
     while len(sekvence) > 0:
@@ -40,13 +41,21 @@ def main():
     number = 14
     sv_data = read_data(file_name, field)
 
-    if sv_data is not None:
-        print(f"Načtená data: {sv_data}")
-        vysledek = linear_search(sv_data, number)
-        print(f"Číslo {number} nalezeno sekvencne na indexech: {vysledek}")
-        bin_ser = binary_search(sv_data, number)
-        print(bin_ser)
-
+    print(f"Načtená data: {sv_data}")
+    start = time.perf_counter()
+    vysledek = linear_search(sv_data, number)
+    end = time.perf_counter()
+    print(f"Číslo {number} nalezeno sekvencne na indexech: {vysledek}")
+    b_start = time.perf_counter()
+    bin_ser = binary_search(sv_data, number)
+    b_end = time.perf_counter()
+    print(bin_ser)
+    duration = end - start
+    b_duration = b_end - b_start
+    print(f"Sekvencni hledac bezel {duration:.8f} s")
+    print(f"Binarni hledac bezel {b_duration:.8f} s")
+    a = b_duration/duration * 100
+    print(round(a, 1))
 
 if __name__ == "__main__":
     main()
